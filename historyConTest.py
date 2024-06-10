d2ef9c04e7c10d659b7e4d508304cbf1edacf0bd
Elaine <yylam43-c@my.cityu.edu.hk>
2024-06-10 11:00:11 -0400

    try to store the history on the memory

---
+++ historyConTest.py
@@ -0,0 +1,18 @@
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+from langchain_community.llms import Ollama
+
+
+ollama_llm = Ollama(model = 'llama3')
+memory = ConversationBufferMemory()
+chain = ConversationChain(
+    llm=ollama_llm,
+    memory=memory
+)
+
+while True:
+    msg = input("user: ")
+    if msg.lower() == "exit":
+        break
+    response = chain.invoke(msg)
+    print("bot: " + response)
