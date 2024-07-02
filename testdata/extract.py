8b8c1bd4109e3c867d7807d5c2a6ac6dacab768f
Elaine <yylam43-c@my.cityu.edu.hk>
2024-07-02 16:57:16 -0400

    addd current code set in the dataset

--- testdata/extract.py
+++ testdata/extract.py
@@ -4,15 +4,22 @@
 #     file.writelines('#version_generated: ' + str(version_generated) + '\n')
 #     file.writelines('program:\n' + str(program) + '\n\n\n')
 
-df = {}
+import pandas as pd
+
 with open("./testdata/generateHvScorePrifun.txt", 'r') as file:
     dataset = file.read()
 
-scores = []
+temp = {}
 for data in dataset.split("#score: ")[1:]:
+    program = data[data.find("def priority"):]
+    while True:
+        if program[-1] == '\n':
+            program = program[:-1]
+        else:
+            break
     score = data[data.find("'data2D.txt': ")+14:]
     score = score[:score.find("\n")-1]
-    scores.append(int(scores))
-    
-scores.sort()
-print(scores)
+    temp.__setitem__(program, float(score))
+
+df1 = pd.DataFrame(temp.items(), index=range(len(temp)), columns=['program', 'score'])
+df1.to_excel("output.xlsx")  
