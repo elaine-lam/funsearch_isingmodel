19611b4ddb635e989206775e031697445aee5e47
Elaine <yylam43-c@my.cityu.edu.hk>
2024-07-17 11:52:00 -0400

    reupload

--- funsearch_official_code/temp.py
+++ funsearch_official_code/temp.py
@@ -3,22 +3,21 @@ import evaluate
 import numpy as np
 
 def priority(N, h, J):
-  priorities = np.zeros((N*N,2))
+  priorities = np.zeros((N*N, 2))
   for i in range(N**2):
     site_nbr = (i % N + ((i//N-1)%2 - 1)) % N
-    total_spin = h[site_nbr][i%N] 
-    if J[0,i%N,i//N] > 0:
-      total_spin += sum([J[k,i%N,i//N]*h[(k+N-1)%N][i%N] for k in range(3)])
+    if h[site_nbr][i%N] > 0:
+      h_site = 1
     else:
-      total_spin -= sum([J[k,i%N,i//N]*h[(k+N-1)%N][i%N] for k in range(3)])
-
+      h_site = -1
+    total_spin = J[0, i//N, i//N]*h[i//N][i%N]
+    for k in range(3):
+      total_spin += 2*J[k, i//N, i//N]*h[(k+N-1)%N][i%N]
     if total_spin > 0:
-      priorities[i][0] = total_spin
+      priorities[i][0] = h_site + total_spin
       priorities[i][1] = -priorities[i][0]
     else:
-      priorities[i][0] = -total_spin
-      priorities[i][1] = -priorities[i][0]
-
+      priorities[i][0] = -h_site + total_spin
   return(priorities)
 
 
