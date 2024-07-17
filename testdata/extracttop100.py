2f4646174be72a09fe189ec7ca7b7a1dc3e64b81
abc1233331 <yylam43-c@my.cityu.edu.hk>
2024-07-17 12:02:56 -0400

    little update for the code.txt

--- testdata/extracttop100.py
+++ testdata/extracttop100.py
@@ -5,12 +5,14 @@ name = "code.txt"
 name2 = "script.txt"
 
 with open(name, 'w') as file:
+    file.writelines('{')
     for i in range(len(df["program"])):
         if i == len(df["program"])-1:
             with open(name2, 'w') as file1:
                 file1.writelines(df["program"][i] + '\n')
             break
-        file.writelines("#score: " + str(df["score"][i]))
-        file.writelines("\n" + df["program"][i] + '\n\n')
+        file.writelines(',{"prompt":"#score: '+ str(df["score"][i]))
+        file.writelines('","completion":"' + df["program"][i] + '"}\n')
+    file.writelines('}')
     
 
