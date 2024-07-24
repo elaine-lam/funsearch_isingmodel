ceb48a9c020d54d1f834321382d018446a89e825
Elaine <yylam43-c@my.cityu.edu.hk>
2024-07-24 09:49:40 -0400

    7/24 cap set generated code

--- testdata/cap/extractcode.py
+++ testdata/cap/extractcode.py
@@ -1,22 +1,22 @@
 from datetime import date
 import pandas as pd
 
-# with open("./testdata/generatedPrifun.txt", 'r') as file:
-#   dataset = file.read()
+with open("./testdata/generatedPrifun.txt", 'r') as file:
+  dataset = file.read()
 
 temp = {}
-# for data in dataset.split("#score: ")[1:]:
-#   program = data[data.find("def priority"):]
-#   while True:
-#     if program[-1] == '\n':
-#       program = program[:-1]
-#     else:
-#       break
-#   score = data[data.find("'data2D.txt': ")+14:]
-#   score = score[:score.find("\n")-1]
-#   temp.__setitem__(program, float(score))
+for data in dataset.split("#score: ")[1:]:
+  program = data[data.find("def priority"):]
+  while True:
+    if program[-1] == '\n':
+      program = program[:-1]
+    else:
+      break
+  score = data[data.find("'data2D.txt': ")+14:]
+  score = score[:score.find("\n")-1]
+  temp.__setitem__(program, float(score))
 
-file2 = "./testdata/2024-07-23generateHvScorePrifun.txt"
+file2 = "./testdata/cap/2024-07-24generateHvScorePrifun.txt"
 with open(file2, 'r') as file:
   dataset2 = file.read()
 
@@ -27,11 +27,11 @@ for data in dataset2.split("#score: ")[1:]:
       program = program[:-1]
     else:
       break
-  score = data[data.find("'n8_size512.txt': ")+19:]
+  score = data[data.find("'n8_size512.txt': ")+18:]
   score = score[:score.find("\n")-1]
-  temp.__setitem__(program, float(score))
+  temp.__setitem__(program, int(score))
 
-sorted_temp = sorted(temp.items(), key=lambda x:x[1])
+sorted_temp = sorted(temp.items(), key=lambda x:x[1],reverse=True)
 
 df1 = pd.DataFrame(sorted_temp, index=range(len(sorted_temp)), columns=['program', 'score'])
 name = "./generatedCode/cap/" + date.today().strftime("%m-%d") + "code.xlsx"
